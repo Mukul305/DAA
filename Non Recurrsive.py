5e5a22098b506b23e7769c54d8d77d907d8de9d7
Mukul Chaudhari <95848991+Mukul305@users.noreply.github.com>
2022-11-21 06:56:04 +0530

    Add files via upload

---
+++ Non Recurrsive.py
@@ -0,0 +1,13 @@
+def fib(n):
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+    fibs = [1, 1]
+    for _ in range(2, n):
+        fibs.append(fibs[-1] + fibs[-2])
+    return fibs
+print(fib(1))  
+print(fib(2))  
+print(fib(3))  
+print(fib(4))
